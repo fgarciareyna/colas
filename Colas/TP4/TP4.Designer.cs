233e79bd896786a9e48a8bf42b51bb1c5bfef3a8
Facundo García Reyna <facu.garcia@gmail.com>
2017-10-14 00:21:22 -0300

    Parámetros

---
+++ Colas/TP4/TP4.Designer.cs
@@ -0,0 +1,330 @@
+﻿namespace TP4
+{
+    partial class TP4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gb_llegadas = new System.Windows.Forms.GroupBox();
+            this.lbl_llegadas_lambda = new System.Windows.Forms.Label();
+            this.txt_llegadas_lambda = new System.Windows.Forms.TextBox();
+            this.txt_recepcion_a = new System.Windows.Forms.TextBox();
+            this.lbl_recepcion_a = new System.Windows.Forms.Label();
+            this.gb_recepcion = new System.Windows.Forms.GroupBox();
+            this.txt_recepcion_b = new System.Windows.Forms.TextBox();
+            this.lbl_recepcion_b = new System.Windows.Forms.Label();
+            this.gb_balanza = new System.Windows.Forms.GroupBox();
+            this.txt_balanza_b = new System.Windows.Forms.TextBox();
+            this.lbl_balanza_b = new System.Windows.Forms.Label();
+            this.txt_balanza_a = new System.Windows.Forms.TextBox();
+            this.lbl_balanza_a = new System.Windows.Forms.Label();
+            this.gb_darsenas = new System.Windows.Forms.GroupBox();
+            this.txt_darsenas_b = new System.Windows.Forms.TextBox();
+            this.lbl_darsenas_b = new System.Windows.Forms.Label();
+            this.txt_darsenas_a = new System.Windows.Forms.TextBox();
+            this.lbl_darsenas_a = new System.Windows.Forms.Label();
+            this.gb_recalibracion = new System.Windows.Forms.GroupBox();
+            this.txt_recal_desvest = new System.Windows.Forms.TextBox();
+            this.txt_recalibracion_desvest = new System.Windows.Forms.Label();
+            this.txt_recalibracion_media = new System.Windows.Forms.TextBox();
+            this.lbl_recalibracion_media = new System.Windows.Forms.Label();
+            this.gb_llegadas.SuspendLayout();
+            this.gb_recepcion.SuspendLayout();
+            this.gb_balanza.SuspendLayout();
+            this.gb_darsenas.SuspendLayout();
+            this.gb_recalibracion.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gb_llegadas
+            // 
+            this.gb_llegadas.Controls.Add(this.txt_llegadas_lambda);
+            this.gb_llegadas.Controls.Add(this.lbl_llegadas_lambda);
+            this.gb_llegadas.Location = new System.Drawing.Point(23, 17);
+            this.gb_llegadas.Name = "gb_llegadas";
+            this.gb_llegadas.Size = new System.Drawing.Size(125, 63);
+            this.gb_llegadas.TabIndex = 0;
+            this.gb_llegadas.TabStop = false;
+            this.gb_llegadas.Text = "Llegadas:";
+            // 
+            // lbl_llegadas_lambda
+            // 
+            this.lbl_llegadas_lambda.AutoSize = true;
+            this.lbl_llegadas_lambda.Location = new System.Drawing.Point(17, 25);
+            this.lbl_llegadas_lambda.Name = "lbl_llegadas_lambda";
+            this.lbl_llegadas_lambda.Size = new System.Drawing.Size(48, 13);
+            this.lbl_llegadas_lambda.TabIndex = 0;
+            this.lbl_llegadas_lambda.Text = "Lambda:";
+            // 
+            // txt_llegadas_lambda
+            // 
+            this.txt_llegadas_lambda.Location = new System.Drawing.Point(71, 22);
+            this.txt_llegadas_lambda.Name = "txt_llegadas_lambda";
+            this.txt_llegadas_lambda.Size = new System.Drawing.Size(37, 20);
+            this.txt_llegadas_lambda.TabIndex = 1;
+            this.txt_llegadas_lambda.Text = "7.5";
+            // 
+            // txt_recepcion_a
+            // 
+            this.txt_recepcion_a.Location = new System.Drawing.Point(71, 22);
+            this.txt_recepcion_a.Name = "txt_recepcion_a";
+            this.txt_recepcion_a.Size = new System.Drawing.Size(37, 20);
+            this.txt_recepcion_a.TabIndex = 1;
+            this.txt_recepcion_a.Text = "3";
+            // 
+            // lbl_recepcion_a
+            // 
+            this.lbl_recepcion_a.AutoSize = true;
+            this.lbl_recepcion_a.Location = new System.Drawing.Point(17, 25);
+            this.lbl_recepcion_a.Name = "lbl_recepcion_a";
+            this.lbl_recepcion_a.Size = new System.Drawing.Size(17, 13);
+            this.lbl_recepcion_a.TabIndex = 0;
+            this.lbl_recepcion_a.Text = "A:";
+            // 
+            // gb_recepcion
+            // 
+            this.gb_recepcion.Controls.Add(this.txt_recepcion_b);
+            this.gb_recepcion.Controls.Add(this.lbl_recepcion_b);
+            this.gb_recepcion.Controls.Add(this.txt_recepcion_a);
+            this.gb_recepcion.Controls.Add(this.lbl_recepcion_a);
+            this.gb_recepcion.Location = new System.Drawing.Point(23, 86);
+            this.gb_recepcion.Name = "gb_recepcion";
+            this.gb_recepcion.Size = new System.Drawing.Size(125, 85);
+            this.gb_recepcion.TabIndex = 2;
+            this.gb_recepcion.TabStop = false;
+            this.gb_recepcion.Text = "Recepción:";
+            // 
+            // txt_recepcion_b
+            // 
+            this.txt_recepcion_b.Location = new System.Drawing.Point(71, 48);
+            this.txt_recepcion_b.Name = "txt_recepcion_b";
+            this.txt_recepcion_b.Size = new System.Drawing.Size(37, 20);
+            this.txt_recepcion_b.TabIndex = 3;
+            this.txt_recepcion_b.Text = "7";
+            // 
+            // lbl_recepcion_b
+            // 
+            this.lbl_recepcion_b.AutoSize = true;
+            this.lbl_recepcion_b.Location = new System.Drawing.Point(17, 51);
+            this.lbl_recepcion_b.Name = "lbl_recepcion_b";
+            this.lbl_recepcion_b.Size = new System.Drawing.Size(17, 13);
+            this.lbl_recepcion_b.TabIndex = 2;
+            this.lbl_recepcion_b.Text = "B:";
+            // 
+            // gb_balanza
+            // 
+            this.gb_balanza.Controls.Add(this.txt_balanza_b);
+            this.gb_balanza.Controls.Add(this.lbl_balanza_b);
+            this.gb_balanza.Controls.Add(this.txt_balanza_a);
+            this.gb_balanza.Controls.Add(this.lbl_balanza_a);
+            this.gb_balanza.Location = new System.Drawing.Point(23, 177);
+            this.gb_balanza.Name = "gb_balanza";
+            this.gb_balanza.Size = new System.Drawing.Size(125, 85);
+            this.gb_balanza.TabIndex = 4;
+            this.gb_balanza.TabStop = false;
+            this.gb_balanza.Text = "Balanza:";
+            // 
+            // txt_balanza_b
+            // 
+            this.txt_balanza_b.Location = new System.Drawing.Point(71, 48);
+            this.txt_balanza_b.Name = "txt_balanza_b";
+            this.txt_balanza_b.Size = new System.Drawing.Size(37, 20);
+            this.txt_balanza_b.TabIndex = 3;
+            this.txt_balanza_b.Text = "7";
+            // 
+            // lbl_balanza_b
+            // 
+            this.lbl_balanza_b.AutoSize = true;
+            this.lbl_balanza_b.Location = new System.Drawing.Point(17, 51);
+            this.lbl_balanza_b.Name = "lbl_balanza_b";
+            this.lbl_balanza_b.Size = new System.Drawing.Size(17, 13);
+            this.lbl_balanza_b.TabIndex = 2;
+            this.lbl_balanza_b.Text = "B:";
+            // 
+            // txt_balanza_a
+            // 
+            this.txt_balanza_a.Location = new System.Drawing.Point(71, 22);
+            this.txt_balanza_a.Name = "txt_balanza_a";
+            this.txt_balanza_a.Size = new System.Drawing.Size(37, 20);
+            this.txt_balanza_a.TabIndex = 1;
+            this.txt_balanza_a.Text = "5";
+            // 
+            // lbl_balanza_a
+            // 
+            this.lbl_balanza_a.AutoSize = true;
+            this.lbl_balanza_a.Location = new System.Drawing.Point(17, 25);
+            this.lbl_balanza_a.Name = "lbl_balanza_a";
+            this.lbl_balanza_a.Size = new System.Drawing.Size(17, 13);
+            this.lbl_balanza_a.TabIndex = 0;
+            this.lbl_balanza_a.Text = "A:";
+            // 
+            // gb_darsenas
+            // 
+            this.gb_darsenas.Controls.Add(this.txt_darsenas_b);
+            this.gb_darsenas.Controls.Add(this.lbl_darsenas_b);
+            this.gb_darsenas.Controls.Add(this.txt_darsenas_a);
+            this.gb_darsenas.Controls.Add(this.lbl_darsenas_a);
+            this.gb_darsenas.Location = new System.Drawing.Point(23, 268);
+            this.gb_darsenas.Name = "gb_darsenas";
+            this.gb_darsenas.Size = new System.Drawing.Size(125, 85);
+            this.gb_darsenas.TabIndex = 5;
+            this.gb_darsenas.TabStop = false;
+            this.gb_darsenas.Text = "Dársenas:";
+            // 
+            // txt_darsenas_b
+            // 
+            this.txt_darsenas_b.Location = new System.Drawing.Point(71, 48);
+            this.txt_darsenas_b.Name = "txt_darsenas_b";
+            this.txt_darsenas_b.Size = new System.Drawing.Size(37, 20);
+            this.txt_darsenas_b.TabIndex = 3;
+            this.txt_darsenas_b.Text = "25";
+            // 
+            // lbl_darsenas_b
+            // 
+            this.lbl_darsenas_b.AutoSize = true;
+            this.lbl_darsenas_b.Location = new System.Drawing.Point(17, 51);
+            this.lbl_darsenas_b.Name = "lbl_darsenas_b";
+            this.lbl_darsenas_b.Size = new System.Drawing.Size(17, 13);
+            this.lbl_darsenas_b.TabIndex = 2;
+            this.lbl_darsenas_b.Text = "B:";
+            // 
+            // txt_darsenas_a
+            // 
+            this.txt_darsenas_a.Location = new System.Drawing.Point(71, 22);
+            this.txt_darsenas_a.Name = "txt_darsenas_a";
+            this.txt_darsenas_a.Size = new System.Drawing.Size(37, 20);
+            this.txt_darsenas_a.TabIndex = 1;
+            this.txt_darsenas_a.Text = "20";
+            // 
+            // lbl_darsenas_a
+            // 
+            this.lbl_darsenas_a.AutoSize = true;
+            this.lbl_darsenas_a.Location = new System.Drawing.Point(17, 25);
+            this.lbl_darsenas_a.Name = "lbl_darsenas_a";
+            this.lbl_darsenas_a.Size = new System.Drawing.Size(17, 13);
+            this.lbl_darsenas_a.TabIndex = 0;
+            this.lbl_darsenas_a.Text = "A:";
+            // 
+            // gb_recalibracion
+            // 
+            this.gb_recalibracion.Controls.Add(this.txt_recal_desvest);
+            this.gb_recalibracion.Controls.Add(this.txt_recalibracion_desvest);
+            this.gb_recalibracion.Controls.Add(this.txt_recalibracion_media);
+            this.gb_recalibracion.Controls.Add(this.lbl_recalibracion_media);
+            this.gb_recalibracion.Location = new System.Drawing.Point(23, 359);
+            this.gb_recalibracion.Name = "gb_recalibracion";
+            this.gb_recalibracion.Size = new System.Drawing.Size(125, 85);
+            this.gb_recalibracion.TabIndex = 6;
+            this.gb_recalibracion.TabStop = false;
+            this.gb_recalibracion.Text = "Recalibración:";
+            // 
+            // txt_recal_desvest
+            // 
+            this.txt_recal_desvest.Location = new System.Drawing.Point(71, 48);
+            this.txt_recal_desvest.Name = "txt_recal_desvest";
+            this.txt_recal_desvest.Size = new System.Drawing.Size(37, 20);
+            this.txt_recal_desvest.TabIndex = 3;
+            this.txt_recal_desvest.Text = "7.5";
+            // 
+            // txt_recalibracion_desvest
+            // 
+            this.txt_recalibracion_desvest.AutoSize = true;
+            this.txt_recalibracion_desvest.Location = new System.Drawing.Point(17, 51);
+            this.txt_recalibracion_desvest.Name = "txt_recalibracion_desvest";
+            this.txt_recalibracion_desvest.Size = new System.Drawing.Size(53, 13);
+            this.txt_recalibracion_desvest.TabIndex = 2;
+            this.txt_recalibracion_desvest.Text = "Desv Est:";
+            // 
+            // txt_recalibracion_media
+            // 
+            this.txt_recalibracion_media.Location = new System.Drawing.Point(71, 22);
+            this.txt_recalibracion_media.Name = "txt_recalibracion_media";
+            this.txt_recalibracion_media.Size = new System.Drawing.Size(37, 20);
+            this.txt_recalibracion_media.TabIndex = 1;
+            this.txt_recalibracion_media.Text = "10";
+            // 
+            // lbl_recalibracion_media
+            // 
+            this.lbl_recalibracion_media.AutoSize = true;
+            this.lbl_recalibracion_media.Location = new System.Drawing.Point(17, 25);
+            this.lbl_recalibracion_media.Name = "lbl_recalibracion_media";
+            this.lbl_recalibracion_media.Size = new System.Drawing.Size(39, 13);
+            this.lbl_recalibracion_media.TabIndex = 0;
+            this.lbl_recalibracion_media.Text = "Media:";
+            // 
+            // TP4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(912, 470);
+            this.Controls.Add(this.gb_recalibracion);
+            this.Controls.Add(this.gb_darsenas);
+            this.Controls.Add(this.gb_balanza);
+            this.Controls.Add(this.gb_recepcion);
+            this.Controls.Add(this.gb_llegadas);
+            this.Name = "TP4";
+            this.Text = "Colas";
+            this.gb_llegadas.ResumeLayout(false);
+            this.gb_llegadas.PerformLayout();
+            this.gb_recepcion.ResumeLayout(false);
+            this.gb_recepcion.PerformLayout();
+            this.gb_balanza.ResumeLayout(false);
+            this.gb_balanza.PerformLayout();
+            this.gb_darsenas.ResumeLayout(false);
+            this.gb_darsenas.PerformLayout();
+            this.gb_recalibracion.ResumeLayout(false);
+            this.gb_recalibracion.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gb_llegadas;
+        private System.Windows.Forms.TextBox txt_llegadas_lambda;
+        private System.Windows.Forms.Label lbl_llegadas_lambda;
+        private System.Windows.Forms.TextBox txt_recepcion_a;
+        private System.Windows.Forms.Label lbl_recepcion_a;
+        private System.Windows.Forms.GroupBox gb_recepcion;
+        private System.Windows.Forms.TextBox txt_recepcion_b;
+        private System.Windows.Forms.Label lbl_recepcion_b;
+        private System.Windows.Forms.GroupBox gb_balanza;
+        private System.Windows.Forms.TextBox txt_balanza_b;
+        private System.Windows.Forms.Label lbl_balanza_b;
+        private System.Windows.Forms.TextBox txt_balanza_a;
+        private System.Windows.Forms.Label lbl_balanza_a;
+        private System.Windows.Forms.GroupBox gb_darsenas;
+        private System.Windows.Forms.TextBox txt_darsenas_b;
+        private System.Windows.Forms.Label lbl_darsenas_b;
+        private System.Windows.Forms.TextBox txt_darsenas_a;
+        private System.Windows.Forms.Label lbl_darsenas_a;
+        private System.Windows.Forms.GroupBox gb_recalibracion;
+        private System.Windows.Forms.TextBox txt_recal_desvest;
+        private System.Windows.Forms.Label txt_recalibracion_desvest;
+        private System.Windows.Forms.TextBox txt_recalibracion_media;
+        private System.Windows.Forms.Label lbl_recalibracion_media;
+    }
+}
+
---
+++ Colas/TP4/TP4.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace TP4
+{
+    public partial class TP4 : Form
+    {
+        public TP4()
+        {
+            InitializeComponent();
+        }
+    }
+}
